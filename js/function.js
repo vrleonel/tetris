9b6c83f9c77479d0a71525c301dc20149f00a1eb
Vitor Leonel <vleonel@abril.com.br>
2012-09-05 11:21:15 -0300

    Otimizacao do JS e mais uma peca criada

--- js/function.js
+++ js/function.js
@@ -6,24 +6,49 @@
 // 65 A
 
 var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-q = 31;
+sq = 31;
+
 
 $(document).ready(function(){
   $("button").click(function(event){
     spear();
   });
+  
+  function game_screen(){
+    $("#screen").height(sq*20);
+    $("#screen").width(sq*10);
+  }
+  
+  
+  function spear(){ 
+    l = 4*sq; 
+    t = -sq;
+    
+    for (i=0; i<4; i++){
+      $('<div>')
+      .addClass('sq red')
+      .appendTo('#screen')
+      .css({ top: t+=sq, left: l });
+    }
+  }
 
-  function spear(){
-    l = 4*q; 
-    t = -q; 
-    $(".square-1").each(function(a){   
-      $(this).css({ top: t+=q, left: l });
-      //console.log($(this).position().top);
-    });
+  function square(){
+    l = 0; 
+    t = 0;
+    $('<div>').addClass('sq blue').appendTo('#screen').css({ top: t, left: l });
+    $('<div>').addClass('sq blue').appendTo('#screen').css({ top: t+sq, left: l });
+    $('<div>').addClass('sq blue').appendTo('#screen').css({ top: t, left: l+sq });
+    $('<div>').addClass('sq blue').appendTo('#screen').css({ top: t+sq, left: l+sq });
     
   }
 
+  function sneak(){
+    
+  }
+
+  game_screen();
   spear();
+  square();
   
 });
 
@@ -31,27 +56,28 @@ $(document).ready(function(){
 $(document).keydown(function(e) {
   //alert(e.keyCode);
   
-  if(e.keyCode == 40){
-    $(".square-1").each(function(a){
-      top_now = $(this).position().top;
-      left_now = $(this).position().left;
-      $(this).css({ top: top_now+q, left: left_now });
+  piece = $(".red");
+  
+  if(e.keyCode == 40){ // Down
+    piece.each(function(a){
+      pos = $(this).position();
+      $(this).css({ top: pos.top+sq, left: pos.left, of: "#screen", collision: "fit" });
     });
   }
   
-  if(e.keyCode == 37){
-    $(".square-1").each(function(a){
+  if(e.keyCode == 37){ // left
+    piece.each(function(a){
       top_now = $(this).position().top;
       left_now = $(this).position().left;
-      $(this).css({ top: top_now, left: left_now-q });
+      $(this).css({ top: top_now, left: left_now-sq });
     });
   }
 
-  if(e.keyCode == 39){
-    $(".square-1").each(function(a){
+  if(e.keyCode == 39){ // Right
+    piece.each(function(a){
       top_now = $(this).position().top;
       left_now = $(this).position().left;
-      $(this).css({ top: top_now, left: left_now+q });
+      $(this).css({ top: top_now, left: left_now+sq });
     });
   }
   
