218970f2616b8afd4f5ea112449432ee22a0f6be
Vitor Leonel <vleonel@abril.com.br>
2012-09-09 00:23:08 -0300

    Pecas Rotacionando

--- js/function.js
+++ js/function.js
@@ -1,6 +1,7 @@
 var sq = 31;
 var land = "#screen";
 var rotate = 0;
+var type = "spear";
 
 function game_screen(){ // Cria a tela do jogo no tamanho certo
   $("#screen").height(sq*20);
@@ -15,13 +16,48 @@ function getPositions(){ // Pega a posição atual da peca e adiciona a um array
   });
 }
 
+var json = 
+  {"spear": {
+      0: [{top: 0, left: 0},{top: 1, left: 0},{top: 2, left: 0},{top: 3, left: 0}],
+      1: [{top: 0, left: 0},{top: 0, left: 1},{top: 0, left: 2},{top: 0, left: 3}]
+    },
+    "square": { 
+      0: [{top: 0, left: 0},{top: 1, left: 0},{top: 0, left: 1},{top: 1, left: 1}]
+    },
+    "hookL": {
+      0: [{top: 0, left: 0},{top: 0, left: 1},{top: 1, left: 1},{top: 2, left: 1}],
+      1: [{top: 0, left: 2},{top: 1, left: 2},{top: 1, left: 1},{top: 1, left: 0}],
+      2: [{top: 0, left: 0},{top: 1, left: 0},{top: 2, left: 0},{top: 2, left: 1}],
+      3: [{top: 0, left: 0},{top: 1, left: 0},{top: 0, left: 1},{top: 0, left: 2}]
+      
+    },
+    "hookR": { 
+      0: [{top: 0, left: 0},{top: 1, left: 0},{top: 2, left: 0},{top: 0, left: 1}],
+      1: [{top: 0, left: 0},{top: 0, left: 1},{top: 0, left: 2},{top: 1, left: 2}],
+      2: [{top: 0, left: 1},{top: 1, left: 1},{top: 2, left: 1},{top: 2, left: 0}],
+      3: [{top: 0, left: 0},{top: 1, left: 0},{top: 1, left: 1},{top: 1, left: 2}]
+    },
+    "sneakL": { 
+      0: [{top: 0, left: 0},{top: 1, left: 0},{top: 1, left: 1},{top: 1, left: 2}]
+    },
+    "sneakR": { 
+      0: [{top: 0, left: 1},{top: 0, left: 2},{top: 1, left: 0},{top: 1, left: 1}]
+    },
+    "arrow": { 
+      0: [{top: 0, left: 1},{top: 1, left: 0},{top: 1, left: 1},{top: 1, left: 2}]
+    },
+    
+}; 
+
+console.log(json.spear);
+
+
 function spear(name){ //Cria a peca spear
   var positions = [];
   positions[0] = [{top: 0, left: 0},{top: 1, left: 0},{top: 2, left: 0},{top: 3, left: 0}];
   positions[1] = [{top: 0, left: 0},{top: 0, left: 1},{top: 0, left: 2},{top: 0, left: 3}];
-  createPiece(name, positions[rotate], "blue");  
+  createPiece(name, positions[0], "blue");  
 }
-
 
 
 function square(name){
@@ -41,7 +77,7 @@ function hookR(name){
 
 function sneakL(name){
   var positions = [{top: 0, left: 0},{top: 1, left: 0},{top: 1, left: 1},{top: 1, left: 2}];
-  createPiece(name, positions, "orange");
+  createPiece(name, positions, "yellow");
 }
 
 function sneakR(name){
@@ -61,41 +97,39 @@ function createPiece(name, positions, color){
   });
 }
 
-$.fn.rotate = function(sum){
-  
-  this.children().each(function(a, i){
-    //console.log(this.style.left);
-  });
-}
+
 /* 
  Teclas
   38: cima,  40 baixo, 37 esquerda, 39 direita 
 */
 
- function key_d(piece){
-   pos = piece.position();
-   piece.css({ top: pos.top+sq, left: pos.left});
-   
- }
- function key_l(piece){
-   pos = piece.position();
-   piece.css({ top: pos.top, left: pos.left-sq});
- }
- function key_r(piece){
-   pos = piece.position();
-   piece.css({ top: pos.top, left: pos.left+sq});
- }
- function key_up(){}
+
+$.fn.rotate = function(){
+
+  if(json[type][++rotate] === undefined){ rotate=0; }
+  var pos = json[type][rotate];
+  this.children().each(function(a, i){
+    $(this).css({top: pos[a].top*sq, left: pos[a].left*sq });
+  });
+}
+
+function key_d(piece){
+  pos = piece.position();
+  piece.css({ top: pos.top+sq, left: pos.left});
+ 
+}
+function key_l(piece){
+  pos = piece.position();
+  piece.css({ top: pos.top, left: pos.left-sq});
+}
+function key_r(piece){
+  pos = piece.position();
+  piece.css({ top: pos.top, left: pos.left+sq});
+}
+
 
 $(document).ready(function(){  
 
-  json = {"spear": { 0: [{top: 0, left: 0},{top: 1, left: 0},{top: 2, left: 0},{top: 3, left: 0}],
-                     1: [{top: 0, left: 0},{top: 0, left: 1},{top: 0, left: 2},{top: 0, left: 3}]
-          },
-          "square": { 0 : [{top: 0, left: 0},{top: 1, left: 0},{top: 0, left: 1},{top: 1, left: 1}]
-          }
-  }; 
-  console.log(json);
 
   game_screen();
   
@@ -116,8 +150,10 @@ $(document).ready(function(){
   $(".peca-6").css({top: 15*sq, left:0});
   $(".peca-7").css({top: 15*sq, left:3*sq});
   
-  piece = $(".peca-7").css({top: 0, left:4*sq}).appendTo("#screen");
-  piece.rotate(10);
+  
+  createPiece("peca-8", json[type][rotate], "orange");
+  piece = $(".peca-8").css({top: 0, left:4*sq}).appendTo("#screen");
+  //piece.rotate(10);
   getPositions();
 });
 
@@ -141,6 +177,10 @@ $(document).keydown(function(e) {
 
   if(e.keyCode == 39){ // Right
     key_r(piece);
+  }
+  
+  if(e.keyCode == 38){ // Right
+    piece.rotate();
   }
   
   
